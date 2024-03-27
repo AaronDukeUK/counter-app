f70744488ab6a2ebb8af5a9109fc7a67acd5cb2a
aaronduke <hello@aaronduke.co.uk>
2024-03-27 20:53:35 +0000

    moved to /src

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./js/script.js",
+  entry: "./src/js/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist/js"),
