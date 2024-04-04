5e19c9150301c3a1f2607cff9f3d43fd1f2e3545
aaronduke <hello@aaronduke.co.uk>
2024-04-04 22:51:58 +0100

    logo updates, modal margin to

--- src/js/add-to-homescreen.js
+++ src/js/add-to-homescreen.js
@@ -1,7 +1,7 @@
 export const InitializeAddToHomeScreen = () => {
   window.AddToHomeScreenInstance = new window.AddToHomeScreen({
     appName: "Dhikr",
-    appIconUrl: "/assets/images/512x512.png",
+    appIconUrl: "/assets/images/192x192.png",
     assetUrl:
       "https://cdn.jsdelivr.net/gh/philfung/add-to-homescreen@1.8/dist/assets/img/",
     showErrorMessageForUnsupportedBrowsers:
